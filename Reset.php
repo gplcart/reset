824b6d411b0defb04423081874158a72811d4798
GplCart <gplcart.software@gmail.com>
2017-11-06 17:51:56 +0200

    Core API changes

--- Reset.php
+++ Reset.php
@@ -9,7 +9,8 @@
 
 namespace gplcart\modules\reset;
 
-use gplcart\core\Module;
+use gplcart\core\Module,
+    gplcart\core\Config;
 
 /**
  * Main class for Reset module
@@ -18,11 +19,11 @@ class Reset extends Module
 {
 
     /**
-     * Constructor
+     * @param Config $config
      */
-    public function __construct()
+    public function __construct(Config $config)
     {
-        parent::__construct();
+        parent::__construct($config);
     }
 
     /**
